a06e1764f100595d42becf571b62dc9c4e4b2ba9
hbjohnsan <3174689@qq.com>
2017-05-08 11:02:24 +0800

    add test.ph test

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 define("DB_HOST",'localhost');
-define("DB_USER",'root');
-define('DB_PWD','root');
+define("DB_USER",'army');
+define('DB_PWD','dDRvVmfbpD96qJx8');
 define('DB_NAME','army');
 define('DB_PORT','3306');
 define('DB_TYPE','mysql');
